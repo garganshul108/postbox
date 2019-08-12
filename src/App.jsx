b50b443a3c7399b1217e073b130337622e45af64
garganshul108 <sci.agarg@gmail.com>
2019-08-12 18:00:28 +0530

    challange completed

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import http from "./services/httpServices";
+
+class App extends Component {
+  state = { posts: [] };
+  async componentDidMount() {
+    let posts = await http.get("https://jsonplaceholder.typicode.com/posts");
+    this.setState({ posts: posts.data });
+  }
+
+  render() {
+    const displayPosts = posts => {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>TITLE</th>
+              <th>BODY</th>
+            </tr>
+          </thead>
+          <tbody>
+            {posts.map(({ id, title, body }) => (
+              <tr>
+                <td>{id}</td>
+                <td>{title}</td>
+                <td>{body}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+    };
+    return <React.Fragment>{displayPosts(this.state.posts)}</React.Fragment>;
+  }
+}
+
+export default App;
