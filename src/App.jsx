81b5d48ca51bf2a21c7c7148e962494f807fc1df
garganshul108 <sci.agarg@gmail.com>
2019-08-12 18:09:41 +0530

    mimic loading done

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,39 @@ class App extends Component {
   state = { posts: [] };
   async componentDidMount() {
     let posts = await http.get("https://jsonplaceholder.typicode.com/posts");
-    this.setState({ posts: posts.data });
+    setTimeout(() => {
+      this.setState({ posts: posts.data });
+    }, 2000);
   }
 
   render() {
     const displayPosts = posts => {
-      return (
-        <table className="table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>TITLE</th>
-              <th>BODY</th>
-            </tr>
-          </thead>
-          <tbody>
-            {posts.map(({ id, title, body }) => (
+      if (posts.length > 0)
+        return (
+          <table className="table">
+            <thead>
               <tr>
-                <td>{id}</td>
-                <td>{title}</td>
-                <td>{body}</td>
+                <th>ID</th>
+                <th>TITLE</th>
+                <th>BODY</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      );
+            </thead>
+            <tbody>
+              {posts.map(({ id, title, body }) => (
+                <tr>
+                  <td>{id}</td>
+                  <td>{title}</td>
+                  <td>{body}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        );
+      else {
+        return <React.Fragment>Loading...</React.Fragment>;
+      }
     };
+
     return <React.Fragment>{displayPosts(this.state.posts)}</React.Fragment>;
   }
 }
